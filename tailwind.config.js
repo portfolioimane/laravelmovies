56e706038558dfddcf4e051b264e0ac3dd13c429
portfolioimane <portfolioimane.g@gmail.com>
2021-02-12 15:02:44 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      width:{
+        '96': '25rem',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
